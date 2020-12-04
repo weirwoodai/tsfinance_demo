c81ef7da55978b4bc080b3aff5ccd637260de710
Ricard_MBP16 <rikitzzz@gmail.com>
2020-12-04 12:39:40 +0100

    refactor: moved csv conversion to class

---
+++ CSV.ts
@@ -0,0 +1,48 @@
+class CSV {
+  private _separator: string;
+  constructor(separator: string = ',') {
+    this._separator = separator;
+  }
+
+  get separator(): string {
+    return this._separator;
+  }
+
+  set separator(separator: string) {
+    this._separator = separator;
+  }
+
+  convert(data: any[]) {
+    //Get the name of all the properties of this object
+    const headersReference = Object.keys(data[0]);
+
+    //go through all the elements in the Data[] to build arrays that only
+    //contain the values of the properties. The first element of this resulting
+    //array will contain the names of the properties.
+    return data
+      .reduce(
+        (t, d) => {
+          //make sure the headers of all the objects are the same
+          const headers = Object.keys(d);
+          if (!CSV.sameMembers(headersReference, headers)) {
+            throw new Error('Different amount of fields!');
+          }
+          //get the values of this object and construct a string separated by
+          //the 'separator' character defined in this CSV
+          return [...t, Object.values(d).join(this.separator)];
+        },
+        [headersReference.join(this.separator)]
+      )
+      .join('\n');
+  }
+
+  private static containsAll(a: string[], b: string[]) {
+    return b.every((e: any) => a.includes(e));
+  }
+
+  private static sameMembers(a: string[], b: string[]) {
+    return CSV.containsAll(a, b) && CSV.containsAll(b, a);
+  }
+}
+
+export default CSV;
--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import FinTen from 'tsfinance';
 import Filing from 'tsfinance/dist/Filing.interface';
-console.log('Hello, world!');
+import CSV from './CSV';
 
 (async () => {
   try {
@@ -12,45 +12,22 @@ console.log('Hello, world!');
     finten.password = process.env.PASSWORD as string;
 
     await finten.login();
-    console.log(`Token is: ${finten.token}`);
-
     const { tickers } = await finten.getTickers();
     console.log('Tickers:', tickers);
 
     const data: Filing[] = [];
     for (const ticker of tickers) {
       const { filings } = await finten.getFilings(ticker);
-      console.log(`Filings from ${ticker}:`, filings.length);
+      console.log(`${ticker}:`, filings.length, 'filings');
       data.push(...filings);
     }
 
-    const csv = convertToCSV(data);
-
+    console.log('Converting data to CSV...');
+    const csv = new CSV(';').convert(data);
+    console.log('Conversion finished! Writing to "data.csv"...');
     fs.writeFileSync(path.resolve(__dirname, 'data.csv'), csv);
+    console.log('Done writing!');
   } catch (ex) {
     console.error(ex);
   }
 })();
-
-function convertToCSV(data: any[]) {
-  const containsAll = (a: any[], b: any[]) =>
-    b.every((e: any) => a.includes(e));
-
-  const sameMembers = (a: any[], b: any[]) =>
-    containsAll(a, b) && containsAll(b, a);
-
-  const headersReference = Object.keys(data[0]);
-
-  return data
-    .reduce(
-      (t, d) => {
-        const headers = Object.keys(d);
-        if (!sameMembers(headersReference, headers)) {
-          throw new Error('Different amount of fields!');
-        }
-        return [...t, Object.values(d).join(';')];
-      },
-      [headersReference.join(';')]
-    )
-    .join('\n');
-}

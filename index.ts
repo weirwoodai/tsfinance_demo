44f99e1ebec13d055f8198c4a89af00933ba09d6
Ricard_MBP16 <rikitzzz@gmail.com>
2021-01-12 16:13:15 +0100

    docs: added explanations
    Also fixed username and password assignment only in case they are
    present as environment variables.

--- index.ts
+++ index.ts
@@ -6,22 +6,37 @@ import CSV from './CSV';
 
 (async () => {
   try {
+    //Create a new FinTen object (doesn't need initialization)
     const finten = new FinTen();
 
-    finten.username = process.env.USERNAME as string;
-    finten.password = process.env.PASSWORD as string;
+    //add your own credentials to get access to more data
+    if (process.env.USERNAME && process.env.PASSWORD) {
+      finten.username = process.env.USERNAME as string;
+      finten.password = process.env.PASSWORD as string;
+    }
 
+    //ask FinTen to obtain a token
+    //the token will be obtained automatically if none is present, so this call
+    //is not necessary
     await finten.login();
+
+    //request the list of available tickers
+    //the response is a JSON object with a property called 'tickers', so we use
+    //destructuring to get that property alone
     const { tickers } = await finten.getTickers();
     console.log('Tickers:', tickers);
 
     const data: Filing[] = [];
     for (const ticker of tickers) {
+      //for each ticker in the list of tickers, get the filings available
+      //the response is again a JSON object with a 'filings' property, so we use
+      //destructuring again
       const { filings } = await finten.getFilings(ticker);
       console.log(`${ticker}:`, filings.length, 'filings');
       data.push(...filings);
     }
 
+    //convert the filings to CSV and save them to a file
     console.log('Converting data to CSV...');
     const csv = new CSV(';').convert(data);
     console.log('Conversion finished! Writing to "data.csv"...');
